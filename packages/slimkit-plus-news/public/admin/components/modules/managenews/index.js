fb1e6c162d772f449ba369c2ff5b78763599eb4e
曹阳洋 <caoyangyang@shopex.cn>
2018-05-17 14:09:38 +0800

    重置packages包

--- packages/slimkit-plus-news/public/admin/components/modules/managenews/index.js
+++
@@ -1,9 +0,0 @@
-import Editor from "./editor/Editor";
-import TagSelect from "./TagSelect";
-import PostCover from "./PostCover";
-
-export default {
-    "module-editor": Editor,
-    "module-post-cover": PostCover,
-    "module-tag-select": TagSelect,
-};
--- packages/slimkit-plus-news/public/admin/components/modules/newslist/index.js
+++
@@ -1,7 +0,0 @@
-import NewsList from './List';
-import NewsFilters from './NewsFilters';
-
-export default {
-  'module-news-list': NewsList,
-  'module-news-filters': NewsFilters
-};
